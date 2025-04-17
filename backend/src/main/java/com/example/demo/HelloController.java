877f4ad7b1a9cbf2473cf020f27cf6e83be92555
nahong_c <95984922+HongChan1412@users.noreply.github.com>
2025-04-17 11:48:31 +0900

    update

--- backend/src/main/java/com/example/demo/HelloController.java
+++ backend/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, FISA Cloud Engineering Seminar!";
+        return "Hello, FISA Cloud Engineering Seminar! 0417";
     }
 }
